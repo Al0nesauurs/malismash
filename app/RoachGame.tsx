f2b495808ac60a9fa84b23cefca7d50e6cf4cb2a
Parut <Parut_s>
2026-03-22 10:43:39 +0700

    init game

---
+++ app/RoachGame.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import Image from "next/image"
+import { useEffect, useRef, useState } from "react"
+
+export default function RoachGame() {
+  const [position, setPosition] = useState({ top: '50%', left: '50%' })
+  const [isAlive, setIsAlive] = useState(true)
+  const intervalRef = useRef<NodeJS.Timeout | null>(null)
+  const [size, setSize] = useState(200)
+
+  const moveRoach = () => {
+    const randomTop = Math.floor(Math.random() * 80) + 10
+    const randomLeft = Math.floor(Math.random() * 80) + 10
+    setPosition({ top: `${randomTop}%`, left: `${randomLeft}%` })
+  }
+
+  const respawnRoach = () => {
+    moveRoach()
+    setSize(Math.floor(Math.random() * 151) + 200)
+    setIsAlive(true)
+    intervalRef.current = setInterval(moveRoach, 2000)
+    return () => {
+      if (intervalRef.current) clearInterval(intervalRef.current)
+    }
+  }
+
+  useEffect(() => {
+    respawnRoach()
+  }, [])
+
+  const handleSmash = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    if (intervalRef.current) clearInterval(intervalRef.current)
+    setIsAlive(false)
+    setTimeout(() => {
+      respawnRoach()
+    }, 5000)
+  }
+
+  return (
+    <div
+      onClick={handleSmash}
+      className={`absolute ${isAlive ? 'transition-all duration-150 ease-in-out' : 'duration-0'} cursor-pointer text-5xl select-none`}
+      style={{
+        top: position.top,
+        left: position.left,
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+
+      <div >
+        {isAlive ?
+          <Image
+            src="/roach.png"
+            alt="Roach"
+            width={size}
+            height={size}
+          />
+          : <Image
+            src="/roachDead2.png"
+            alt="Roach"
+            width={size}
+            height={size} />
+        }
+      </div>
+    </div>
+  )
+}
